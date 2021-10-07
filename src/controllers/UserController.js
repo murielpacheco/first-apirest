e831dcfa6251611b6b269cd9069b10fd360adc4c
Muriel Damazio <murieldps@gmail.com>
2021-10-07 10:27:02 -0300

    CRUD users done

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -15,6 +15,76 @@ class UserController {
       });
     }
   }
+
+  // Index
+  async index(req, res) {
+    try {
+      const users = await User.findAll();
+      return res.json(users);
+    } catch (error) {
+      return res.json(null);
+    }
+  }
+  // Show
+
+  async show(req, res) {
+    try {
+      const user = await User.findByPk(req.params.id);
+      return res.json(user);
+    } catch (error) {
+      return res.json(null);
+    }
+  }
+  // Update
+
+  async update(req, res) {
+    try {
+      if (!req.params.id) {
+        return res.status(400).json({
+          errors: ['ID não enviado'],
+        });
+      }
+
+      const user = await User.findByPk(req.params.id);
+
+      if (!user) {
+        return res.status(400).json({
+          errors: ['Usuário não existe'],
+        });
+      }
+
+      const novosDados = await user.update(req.body);
+
+      return res.json(novosDados);
+    } catch (error) {
+      return res.json(null);
+    }
+  }
+
+  // Delete
+  async delete(req, res) {
+    try {
+      if (!req.params.id) {
+        return res.status(400).json({
+          errors: ['ID não enviado'],
+        });
+      }
+
+      const user = await User.findByPk(req.params.id);
+
+      if (!user) {
+        return res.status(400).json({
+          errors: ['Usuário não existe'],
+        });
+      }
+
+      await user.destroy();
+
+      return res.json(user);
+    } catch (error) {
+      return res.json(null);
+    }
+  }
 }
 
 export default new UserController();
--- src/models/User.js
+++ src/models/User.js
@@ -3,47 +3,52 @@ import bcryptjs from 'bcryptjs';
 
 export default class User extends Model {
   static init(sequelize) {
-    super.init({
-      nome: {
-        type: Sequelize.STRING,
-        defaultValue: '',
-        validate: {
-          len: {
-            args: [4, 30],
-            msg: 'Campo nome deve ter entre 4 e 30 caracteres',
+    super.init(
+      {
+        nome: {
+          type: Sequelize.STRING,
+          defaultValue: '',
+          validate: {
+            len: {
+              args: [4, 30],
+              msg: 'Campo nome deve ter entre 4 e 30 caracteres',
+            },
+          },
+        },
+        email: {
+          type: Sequelize.STRING,
+          defaultValue: '',
+          validate: {
+            isEmail: {
+              args: [4, 30],
+              msg: 'Email inválido',
+            },
+          },
+        },
+        password_hash: {
+          type: Sequelize.STRING,
+          defaultValue: '',
+        },
+        password: {
+          type: Sequelize.VIRTUAL,
+          defaultValue: '',
+          validate: {
+            len: {
+              args: [6, 30],
+              msg: 'A senha deve ter entre 6 e 30 caracteres',
+            },
           },
         },
       },
-      email: {
-        type: Sequelize.STRING,
-        defaultValue: '',
-        validate: {
-          isEmail: {
-            args: [4, 30],
-            msg: 'Email inválido',
-          },
-        },
+      {
+        sequelize,
       },
-      password_hash: {
-        type: Sequelize.STRING,
-        defaultValue: '',
-      },
-      password: {
-        type: Sequelize.VIRTUAL,
-        defaultValue: '',
-        validate: {
-          len: {
-            args: [6, 30],
-            msg: 'A senha deve ter entre 6 e 30 caracteres',
-          },
-        },
-      },
-    }, {
-      sequelize,
-    });
+    );
 
     this.addHook('beforeSave', async (user) => {
-      user.password_hash = await bcryptjs.hash(user.password, 8);
+      if (user.password) {
+        user.password_hash = await bcryptjs.hash(user.password, 8);
+      }
     });
 
     return this;
